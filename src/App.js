ef81988e6057e76988e14a4ca7d7071915571dc0
Navjot Singh Chahal <navjot.chahal@rallyhealth.com>
2022-07-07 15:52:57 +0530

    added react-router-dom setup routing setup signin component set up navigation bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from './routes/homepage/homepage.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 function App() {
   return (
-    <div>
-      <HomePage/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Navigation/>} >
+        <Route index element={<HomePage></HomePage>}></Route>
+        <Route path='shop' ></Route>
+        <Route path='sign-in' element={<SignIn/>}></Route>
+      </Route>
+    </Routes>
   );
 }
 
