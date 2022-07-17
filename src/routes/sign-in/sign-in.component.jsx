219ce766e9e87a2cfcab7c0eb0ff9933165f897c
navs.code <navjot.chahal@optum.com>
2022-07-17 18:18:45 +0530

    sign vup form completed via e&P
    work in progress Sign in with e&P

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,49 @@
 import { getRedirectResult } from 'firebase/auth';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth, createUserDocumentFromAuth, signInWithGooglePopup, signInWithGoogleRedirect } from '../../utils/firebase/firebase.util';
 import SignUpComponent from '../sign-up/sign-up.component';
+import Button from '../../components/button/button.component';
+import FormInputComponent from '../../components/form-input/form-input.component';
+
+const defaultLoginFormFields = {
+    email: '',
+    password: ''
+}
 
 const SignIn = () => {
+    const [loginFormFields, setLoginFormFields] = useState(defaultLoginFormFields);
+
+    const { email, password } = loginFormFields;
+
+    const resetSigninForm = () => {
+        setLoginFormFields(defaultLoginFormFields);
+    }
+
+    const handleLoginFormChange = (event) => {
+        const {name, value} = event.target;
+        setLoginFormFields({...loginFormFields, [name]: value});
+    }
+
+    // work in progress !
+    const handleLoginFormSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const result = await createAuthUserWithEmailAndPassword(email, password);
+            await createUserDocumentFromAuth(result.user, {displayName});
+            console.log(result);
+            resetSigninForm();
+        } catch (error) {
+            if (error.code === 'auth/email-already-in-use') {
+                alert('User sign-up failed! as email already in use.');
+            } else {
+                console.log('user login via e&p encountered an error ', error.message);
+            }
+        }
+
+    }
+
+
     const logGooglePopupUser = async () => {
         const response = await signInWithGooglePopup();
         console.log(response);
@@ -24,13 +64,19 @@ const SignIn = () => {
 
     return (
         <div className='sign-in-container'>
-            <h1>Sign-In Page</h1>
-            <button onClick={logGooglePopupUser}>
+            <h2>I already have an account</h2>
+            <span>Sign-In with your email and password</span>
+            <form onSubmit={handleLoginFormSubmit}>
+                <FormInputComponent label='Email' type="email" required onChange={handleLoginFormChange} name='email' value={email}/>
+                <FormInputComponent label='Password' type="password" required onChange={handleLoginFormChange} name='password' value={password}/>
+            </form>
+            
+            <Button onClick={logGooglePopupUser}>
                 Sign-In with Google Popup
-            </button>
-            <button onClick={signInWithGoogleRedirect}>
+            </Button>
+            <Button onClick={signInWithGoogleRedirect}>
                 Sign-In with Google Redirect
-            </button>
+            </Button>
             <SignUpComponent></SignUpComponent>
         </div>
     );
--- src/routes/sign-up/sign-up.component.jsx
+++ src/routes/sign-up/sign-up.component.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.util';
+import FormInputComponent from '../../components/form-input/form-input.component'
+import './sign-up.styles.scss';
+import Button from '../../components/button/button.component';
 
 const defaultFormFields = {
     displayName: '',
@@ -39,28 +42,24 @@ const SignUpComponent = () => {
             resetSignUpForm();
         } catch (error) {
             if (error.code === 'auth/email-already-in-use') {
-                alert('User sign-up failed! as email already in use.')
+                alert('User sign-up failed! as email already in use.'); 
             } else {
-                
+                console.log('user creation via e&p encountered an error ', error.message);
             }
-            console.log('user creation via e&p encountered an error ', error.message)
         }
 
     }
 
     return (
-        <div>
-            <h1>Sign-Up with your email and password :</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account ?</h2>
+            <span>Sign-Up with your email and password :</span>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input type="text" required onChange={handleChange} name='displayName' value={displayName}/>
-                <label>Email</label>
-                <input type="email" required onChange={handleChange} name='email' value={email}/>
-                <label>Password</label>
-                <input type="password" required onChange={handleChange} name='password' value={password}/>
-                <label>Confirm Password</label>
-                <input type="password" required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
-                <button type="submit">Sign-Up</button>
+                <FormInputComponent label='Display Name' type="text" required onChange={handleChange} name='displayName' value={displayName}/>
+                <FormInputComponent label='Email' type="email" required onChange={handleChange} name='email' value={email}/>
+                <FormInputComponent label='Password' type="password" required onChange={handleChange} name='password' value={password}/>
+                <FormInputComponent label='Confirm Password' type="password" required onChange={handleChange} name='confirmPassword' value={confirmPassword}/>
+                <Button type="submit">Sign-Up</Button>
             </form>
         </div>
     );
